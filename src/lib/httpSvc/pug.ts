64b4b7d4aac9a443d8d01b16612d4b4a4aa8e230
Balázs Csaba <csaba@balazs-magyar.hu>
2022-03-31 11:54:16 +0200

    feat: show memory infos

--- src/lib/httpSvc/pug.ts
+++ src/lib/httpSvc/pug.ts
@@ -6,6 +6,7 @@ import * as pug from 'pug';
 import { version } from '../../../package.json';
 import * as config from '../../config';
 import logService from './services/logService';
+import { heapUsage, prettyThousand } from '../utility';
 
 export default async (server: FastifyInstance) => {
     server.log.debug('Init template engine');
@@ -20,8 +21,15 @@ export default async (server: FastifyInstance) => {
             serviceName: config.serviceName,
             devMode: !config.isProduction,
             appVersion: version,
-            getUiNotifications: () => logService.getUiNotification(),
             newline: '\n',
+            getUiNotifications: () => logService.getUiNotification(),
+            getMemoryUsageHuman: () => {
+                const usage = heapUsage();
+                return {
+                    used: prettyThousand(usage.used, 2),
+                    limit: prettyThousand(usage.limit, 2),
+                }
+            }
         },
         production: config.isProduction, //cache files?
         options: {
