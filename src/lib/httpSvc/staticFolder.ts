3212d7acc65ffb1ef8a173044e592785322feb5a
Balázs Csaba <csaba@balazs-magyar.hu>
2022-04-08 20:02:38 +0200

    build: remove huge npms

--- src/lib/httpSvc/staticFolder.ts
+++ src/lib/httpSvc/staticFolder.ts
@@ -1,17 +1,15 @@
 /* eslint-disable unicorn/prefer-module */
 import * as path from 'node:path';
-import findupSync = require('findup-sync');
 import { FastifyInstance } from 'fastify';
 import fastifyStatic from 'fastify-static';
 
 import * as config from '../../config';
 
-const folderNodeModules = path.relative(__dirname, findupSync('node_modules', { cwd: process.cwd() }) || '');
 const staticFolders = new Map<string, string>([
     ['/public', '../../public'],
-    ['/assets/template', path.join(folderNodeModules, 'admin-lte/dist')],
-    ['/assets/plugins', path.join(folderNodeModules, 'admin-lte/plugins')],
-    ['/assets/mermaid', path.join(folderNodeModules, 'mermaid/dist')],
+    ['/assets/template', '../../assets/admin-lte/dist'],
+    ['/assets/plugins', '../../assets/admin-lte/plugins'],
+    ['/assets/mermaid', '../../assets/mermaid/dist'],
 ]);
 
 export default async (server: FastifyInstance) => {
